c7509d64f6974b9f8b3e039478452a4780d3bddf
Omkar Ghag <omkarghag@gmail.com>
2018-01-09 20:53:59 +0000

    Update views and setup email in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,14 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  
+    # For email confirmation
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'omkar-photo-app.herokuapp.com', :protocol => 'https'}
+  
+  
+  
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
